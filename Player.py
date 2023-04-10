2d94558fe6475fde9b1a9cb58a193a99a1ac3bbb
MaitreBabouk <ilianleboss974@gmail.com>
2023-04-10 14:45:46 +0400

    mid 2nd day

--- Player.py
+++ Player.py
@@ -5,11 +5,23 @@ from SETTINGS import *
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("assets\Bell.png").convert_alpha()
+        # self.image = pygame.image.load("assets\Bell.png").convert_alpha()
         self.scaled_image = pygame.transform.scale(self.image, (64, 64))
         self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
 
         self.speed = PLAYER_SPEED
+
+        # Idle
+        player_idle_0 = pygame.image.load('assets/animations/player/idle/idle_0.png').convert_alpha()
+        player_idle_1 = pygame.image.load('assets/animations/player/idle/idle_1.png').convert_alpha()
+        player_idle_2 = pygame.image.load('assets/animations/player/idle/idle_2.png').convert_alpha()
+        self.player_idle_frames = [player_idle_0, player_idle_1, player_idle_2]
+        # Walk down
+        player_walkdown_0 = pygame.image.load('assets/animations/player/walkdown/walkdown_0.png').convert_alpha()
+        player_walkdown_1 = pygame.image.load('assets/animations/player/walkdown/walkdown_1.png').convert_alpha()
+        player_walkdown_2 = pygame.image.load('assets/animations/player/walkdown/walkdown_2.png').convert_alpha()
+        player_walkdown_3 = pygame.image.load('assets/animations/player/walkdown/walkdown_3.png').convert_alpha()
+        self.player_walkdown_frames = [player_walkdown_0, player_walkdown_1, player_walkdown_2, player_walkdown_3]
 
     def user_input(self):
         self.velX = 0
@@ -29,6 +41,29 @@ class Player(pygame.sprite.Sprite):
         if self.velX != 0 and self.velY != 0:
             self.velX /= math.sqrt(2)
             self.velY /= math.sqrt(2)
+
+    def animation_controller(self):
+        current_frame = 0
+        frame = None
+        if keys[pygame.K_UP]:
+            pass
+        elif keys[pygame.K_DOWN]:
+            max_frame = len(self.player_walkdown_frames)
+            frame = self.player_walkdown_frames[current_frame]
+        elif keys[pygame.K_LEFT]:
+            pass
+        elif keys[pygame.K_RIGHT]:
+            pass
+        else:
+            max_frame = len(self.player_idle_frames)
+            
+        
+        if current_frame >= max_frame:
+            current_frame = 0
+        else:
+            current_frame +=1
+        
+        return frame
     
     def move(self):
         self.pos += pygame.math.Vector2(self.velX, self.velY)
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import pygame, sys, math, spritesheet
+import pygame, sys, math
 from pygame.locals import *
 from SETTINGS import *
 from Player import Player
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+        self.camera_offset
 
 pygame.init()
 
@@ -11,11 +18,6 @@ pygame.display.set_caption('Bell of the childs')
 clock = pygame.time.Clock()
 
 # Loads images
-player_spritesheet_image = pygame.image.load('assets\BelIdleSpriteSheetl.png').convert_alpha()
-player_spritesheet = spritesheet.Spritesheet(player_spritesheet_image)
-
-# player animation frames
-player_idle_0 = player_spritesheet.get_image(0, 32, 32, 2, (0, 0, 0))
 
 player = Player()
 
@@ -30,7 +32,7 @@ def game():
                 sys.exit()
 
         screen.blit(player.scaled_image, player.pos)
-        screen.blit(player_idle_0, (0, 0))
+
         player.update()
         pygame.display.update()
         clock.tick(FPS)
